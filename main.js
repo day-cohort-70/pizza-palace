a3ac5bd0edcd5fb5c461be490f42e4ef31ed3b76
Steve Brownlee <chortlehoort@gmail.com>
2024-02-14 10:24:32 -0600

    Initial modules created

--- main.js
+++ main.js
@@ -2,3 +2,26 @@
     Pizza Palace project
     ---------------------
 */
+
+
+/*
+    1. Define my data (structure)
+    2. Functions I'll need
+        menu.js
+        -------
+        - menuDisplay
+
+        order.js
+        --------
+        - order
+        - viewOrder
+        - orderPayment
+        - markOrderComplete
+        - emailReceipt
+
+        account.js
+        ----------
+        - viewAccount
+        - createAccount
+    3.
+*/
